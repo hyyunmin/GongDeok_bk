e595f26688815b6991b69d8a428583e90705b8d1
DESKTOP-SPM016U\offic <hyyunmin@gmail.com>
2020-08-27 14:04:56 +0900

    git test

--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,6 @@ namespace GongDeok_bk
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            var test = "수정";
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
